21ee5f4852993ce97d8ea51c83912cbf72ac7b60
Marina Elizarova <seagirl1110@gmail.com>
2024-12-10 13:56:05 +0300

    feat: add get transactions routes

--- routes/balance.js
+++ routes/balance.js
@@ -3,6 +3,7 @@ import setBalance from '../controllers/setBalance.js';
 import addBalance from '../controllers/addBalance.js';
 import addExpense from '../controllers/addExpense.js';
 import getBalance from '../controllers/getBalance.js';
+import getTransactions from '../controllers/getTransactions.js';
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.post('/set-balance', setBalance);
 router.post('/add-balance', addBalance);
 router.post('/add-expense', addExpense);
 router.get('/get-balance/:userId', getBalance);
+router.get('/get-transactions/:userId', getTransactions);
 
 export default router;
